9640ebb9332d33ac078684fa1306dd0e6cc86db3
Bhavesh Hatwar <bhatwar792@gmail.com>
2026-04-18 23:14:04 +0530

    upload file

---
+++ String_problems/03_count_frequency.py
@@ -0,0 +1,14 @@
+def char_frequency(s):
+    freq = {}
+
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+
+    return freq
+
+# Example
+print(char_frequency("banana"))
+print(char_frequency("Apple"))
